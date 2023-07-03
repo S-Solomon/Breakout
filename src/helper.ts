d28f8f74592a269f20875e09ec3499c884298f91
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-07-03 22:49:33 +0100

    import image files for different colored bricks.

---
+++ src/helper.ts
@@ -0,0 +1,6 @@
+import RED_BRICK from './images/brick-red.png';
+import BLUE_BRICK from './images/brick-blue.png';
+import GREEN_BRICK from './images/brick-green.png';
+import PURPLE_BRICK from './images/brick-purple.png';
+
+
