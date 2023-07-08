c9b4a8314531d898dd1772fb24393e198cde925d
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-07-08 23:43:28 +0100

    Typescript changes so quickly that the information gets outdated, sometimes drives me mad

---
+++ declarations.d.ts
@@ -0,0 +1,19 @@
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+
+declare module "*.png" {
+    const value: string;
+    export default value;
+}
+
+declare module "./images/paddle.png" {
+    const value: string;
+    export default value;
+}
+
+declare module "./images/ball.png" {
+    const value: string;
+    export default value;
+}
--- src/images/index.d.ts
+++
@@ -1,7 +0,0 @@
-// Declare images and specify types for them
-// Enables the importing of images as modules, making it very convenient
-
-declare module '*.png' {
-    const value: string;
-    export = value;
-}
