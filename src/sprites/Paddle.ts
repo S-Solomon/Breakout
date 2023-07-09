507c2f6d78ba21f33c279a632ab0d1ac868cdd64
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-07-09 21:19:36 +0100

    Paddle class -- set screen listeners

--- src/sprites/Paddle.ts
+++ src/sprites/Paddle.ts
@@ -1,3 +1,27 @@
 import { Vector } from "../types";
 
-export class Paddle {}
+export class Paddle {
+    private paddleImage: HTMLImageElement = new Image();
+    private moveLeft: boolean;
+    private moveRight: boolean;
+
+    constructor(
+        private speed: number,
+        private paddleWidth: number,
+        private paddleHeight: number,
+        private position: Vector,
+        image: string
+    ) {
+        this.speed = speed;
+        this.paddleWidth = paddleWidth;
+        this.paddleHeight = paddleHeight;
+        this.position = position;
+        this.moveLeft = false;
+        this.moveRight = false;
+        this.paddleImage.src = image;
+
+        // keyboard event listeners
+        document.addEventListener('keydown', this.handleKeyDown);
+        document.addEventListener('keyup', this.handleKeyUp);
+    }
+}
