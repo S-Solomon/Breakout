7ecfbf88499becf82a240b2c50f61248b6509ca5
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-07-09 19:58:06 +0100

    import necesary modules

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+// Import sprites and canvas
+import { CanvasView } from "./view/CanvasView";
+import { Ball } from "./sprites/Ball";
+import { Brick } from "./sprites/Brick";
+import { Paddle } from "./sprites/Paddle";
+
+// images
+import PADDLE_IMAGE from "./images/paddle.png";
+import BALL_IMAGE from "./images/ball.png";
+
+// levels and colours
+import {
+  PADDLE_SPEED,
+  PADDLE_WIDTH,
+  PADDLE_HEIGHT,
+  PADDLE_STARTX,
+  BALL_SPEED,
+  BALL_SIZE,
+  BALL_STARTX,
+  BALL_STARTY,
+} from "./setup";
+
+// helper function
+import { createBricks } from "./helpers";
+
+
