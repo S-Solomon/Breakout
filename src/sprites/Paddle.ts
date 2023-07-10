7c4ae09892ef7201021288ebf5114a2f02d45cd9
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-07-10 05:16:37 +0100

    Getters

--- src/sprites/Paddle.ts
+++ src/sprites/Paddle.ts
@@ -21,7 +21,32 @@ export class Paddle {
         this.paddleImage.src = image;
 
         // keyboard event listeners
-        document.addEventListener('keydown', this.handleKeyDown);
-        document.addEventListener('keyup', this.handleKeyUp);
+        document.addEventListener("keydown", this.handleKeyDown);
+        document.addEventListener("keyup", this.handleKeyUp);
+    }
+
+    // Getters
+    get width(): number {
+        return this.paddleWidth;
+    }
+
+    get height(): number {
+        return this.paddleHeight;
+    }
+
+    get pos(): Vector {
+        return this.position;
+    }
+
+    get image(): HTMLImageElement {
+        return this.paddleImage;
+    }
+
+    get isMovingLeft(): boolean {
+        return this.isMovingLeft;
+    }
+
+    get isMovingRight(): boolean {
+        return this.isMovingRight;
     }
 }
