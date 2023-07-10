937f82aaad2eab1accef6326d75f010a06ad8f95
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-07-10 06:27:42 +0100

    Ball class - constructors

--- src/sprites/Ball.ts
+++ src/sprites/Ball.ts
@@ -1,3 +1,21 @@
 import { Vector } from "../types";
 
-export class Ball {}
+export class Ball {
+    private speed: Vector;
+    private ballImage: HTMLImageElement = new Image();
+
+    constructor(
+        private ballSize: number,
+        private position: Vector,
+        speed: number,
+        image: string
+    ) {
+        this.ballSize = ballSize;
+        this.position = position;
+        this.speed = {
+            x: speed,
+            y: -speed,
+        };
+        this.ballImage.src = image;
+    }
+}
