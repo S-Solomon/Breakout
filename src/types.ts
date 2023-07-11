dab915edd798c5cd05209a76a150e5d99aef5cc9
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-07-11 23:24:21 +0100

    REFACTOR - creat sprite class to provide common properties and methid

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,38 @@ export type Vector = {
   x: number;
   y: number;
 };
+
+export abstract class Sprite {
+  protected position: Vector;
+  protected spriteImage: HTMLImageElement | any = new Image();
+
+  constructor(
+    protected width: number,
+    protected height: number,
+    protected image: string,
+    position: Vector
+  ) {
+    this.width = width;
+    this.height = height;
+    this.position = position;
+    this.spriteImage.src = image;
+  }
+
+  abstract update(): void;
+
+  getWidth(): number {
+    return this.width;
+  }
+
+  getHeight(): number {
+    return this.height;
+  }
+
+  getPosition(): Vector {
+    return this.position;
+  }
+
+  getImage(): HTMLImageElement {
+    return this.spriteImage;
+  }
+}
