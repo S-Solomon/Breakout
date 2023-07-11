5c64def37e4ff9cb8966a57ee31064f5421d8c09
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-07-12 00:13:35 +0100

    REFACTOR - main game class  & instantiate new game

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { Ball } from "./sprites/Ball";
 import { Brick } from "./sprites/Brick";
 import { Paddle } from "./sprites/Paddle";
 import { Collision } from "./Collision";
-
-// images
+import { createBricks } from "./helpers";
 import PADDLE_IMAGE from "./images/paddle.png";
 import BALL_IMAGE from "./images/ball.png";
 
@@ -21,100 +20,100 @@ import {
   BALL_STARTY,
 } from "./setup";
 
-// helper function
-import { createBricks } from "./helpers";
 
+class Breakout {
+  private view: CanvasView;
+  private bricks: Brick[];
+  private paddle: Paddle;
+  private ball: Ball;
+  private collision: Collision;
+  private gameOver: boolean;
+  private score: number;
 
-// Initializing game state variables
-let gameOver = false;
-let score = 0;
-
-// Game over and game win functions:
-function setGameOver(view: CanvasView) {
-  view.drawInfo("Game Over!");
-  gameOver = false;
-}
-function setGameWin(view: CanvasView) {
-  view.drawInfo("Game Won");
-  gameOver = false;
-}
-
-// main game loop function that continuously updates the game view
-function gameLoop(
-  view: CanvasView,
-  bricks: Brick[],
-  paddle: Paddle,
-  ball: Ball,
-  collision: Collision
-) {
-  console.log('drawing!')
-  view.clear(); // clear the canvas
-  view.drawBricks(bricks); // draw bricks
-  view.drawSprite(paddle); // draw paddle
-  view.drawSprite(ball);
-  ball.moveBall();
-  paddle.movePaddle();
-
-  collision.checkBallCollision(ball, paddle, view);
-  const collidingBrick = collision.isCollidingBricks(ball, bricks);
-
-  if (collidingBrick) {
-    score += 1;
-    view.drawScore(score);
-  }
-
-  // Game Over when ball leaves playField
-  if (ball.pos.y > view.canvas.height) gameOver = true;
-  // If game won, set gameOver and display win
-  if (bricks.length === 0) return setGameWin(view);
-  // Return if gameover and don't run the requestAnimationFrame
-  if (gameOver) return setGameOver(view);
-
-
-
-  // schedules the next iteration of the game loop using requestAnimationFrame.
-  requestAnimationFrame(() => gameLoop(view, bricks, paddle, ball, collision));
-}
-
-// This function is called when the game is started
-function startGame(view: CanvasView) {
-  // reset displays
-  score = 0;
-  view.drawInfo("");
-  view.drawScore(0);
-  // new collision instance
-  const collision = new Collision();
-
-  // create all bricks
-  const bricks = createBricks();
-
-  // create ball
-  const ball = new Ball(
+  constructor() {
+    this.view = new CanvasView("#playField");
+    this.bricks = [];
+    this.paddle = new Paddle(
+      PADDLE_SPEED,
+      PADDLE_WIDTH,
+      PADDLE_HEIGHT,
+      {
+        x: PADDLE_STARTX,
+        y: this.view.canvas.height - PADDLE_HEIGHT - 5,
+      },
+      PADDLE_IMAGE,
+      this.view.canvas.width
+    );
+    this.ball = new Ball(
       BALL_SPEED,
       BALL_SIZE,
       {
-          x: BALL_STARTX,
-          y: BALL_STARTY,
+        x: BALL_STARTX,
+        y: BALL_STARTY,
       },
       BALL_IMAGE
-  );
+    );
+    this.collision = new Collision();
+    this.gameOver = false;
+    this.score = 0;
+  }
 
-  // create paddle instance
-  const paddle = new Paddle(
-    PADDLE_SPEED,
-    PADDLE_WIDTH,
-    PADDLE_HEIGHT,
-    {
-      x: PADDLE_STARTX,
-      y: view.canvas.height - PADDLE_HEIGHT - 5,
-    },
-    PADDLE_IMAGE,
-    view.canvas.width
-  );
+  private setGameOver(): void {
+    this.view.drawInfo("Game Over!");
+    this.gameOver = true;
+  }
 
-  gameLoop(view, bricks, paddle, ball, collision);
+  private setGameWin(): void {
+    this.view.drawInfo("Game Won");
+    this.gameOver = true;
+  }
+
+  private gameLoop(): void {
+    this.view.clear(); // clear the canvas
+    this.view.drawBricks(this.bricks); // draw bricks
+    this.view.drawSprite(this.paddle); // draw paddle
+    this.view.drawSprite(this.ball);
+    this.ball.moveBall();
+    this.paddle.movePaddle();
+
+    this.collision.checkBallCollision(this.ball, this.paddle, this.view);
+    const collidingBrick = this.collision.isCollidingBricks(
+      this.ball,
+      this.bricks
+    );
+
+    if (collidingBrick) {
+      this.score += 1;
+      this.view.drawScore(this.score);
+    }
+
+    // Game Over when ball leaves playField
+    if (this.ball.getPosition().y > this.view.canvas.height)
+      this.gameOver = true;
+    // If game won, set gameOver and display win
+    if (this.bricks.length === 0) return this.setGameWin();
+    // Return if game over and don't run the requestAnimationFrame
+    if (this.gameOver) return this.setGameOver();
+
+    requestAnimationFrame(() => this.gameLoop());
+  }
+
+  public startGame(): void {
+    this.score = 0;
+    this.view.drawInfo("");
+    this.view.drawScore(0);
+    this.gameOver = false;
+
+    this.bricks = createBricks();
+
+    this.gameLoop();
+  }
+
+  public initStartButton(): void {
+    this.view.initStartButton(() => this.startGame());
+  }
 }
 
-// create a new instance of canvas view
-const view = new CanvasView("#playField");
-view.initStartButton(startGame);
+
+const game = new Breakout();
+game.initStartButton();
