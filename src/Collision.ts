0d3e84f521e8462457b7130a7384befb3b14ec5c
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-07-12 00:06:44 +0100

    REFACTOR - Collison class for improved readability - use inherited methods

--- src/Collision.ts
+++ src/Collision.ts
@@ -3,56 +3,59 @@ import { Paddle } from "./sprites/Paddle";
 import { Ball } from "./sprites/Ball";
 import { CanvasView } from "./view/CanvasView";
 
-
 export class Collision {
-    isCollidingBrick(ball: Ball, brick: Brick): boolean {
-        if (
-            ball.pos.x < brick.pos.x + brick.width &&
-            ball.pos.x + ball.width > brick.pos.x &&
-            ball.pos.y < brick.pos.y + brick.height &&
-            ball.pos.y + ball.height > brick.pos.y
-        ) {
-            return true;
-        }
-        return false;
+  // Checks if a single brick is colliding with the ball.
+  isCollidingBrick(ball: Ball, brick: Brick): boolean {
+    if (
+      ball.getPosition().x < brick.getPosition().x + brick.getWidth() &&
+      ball.getPosition().x + ball.getWidth() > brick.getPosition().x &&
+      ball.getPosition().y < brick.getPosition().y + brick.getHeight() &&
+      ball.getPosition().y + ball.getHeight() > brick.getPosition().y
+    ) {
+      return true;
     }
+    return false;
+  }
 
-    // Check ball collision with bricks
-    isCollidingBricks(ball: Ball, bricks: Brick[]): boolean {
-        let colliding = false;
+  // Checks if any bricks in an array are colliding with the ball.
+  isCollidingBricks(ball: Ball, bricks: Brick[]): boolean {
+    let colliding = false;
 
-        bricks.forEach((brick, i) => {
-            if (this.isCollidingBrick(ball, brick)) {
-                ball.changeYDirection();
+    bricks.forEach((brick, index) => {
+      if (this.isCollidingBrick(ball, brick)) {
+        ball.changeYDirection();
 
-                if (brick.energy === 1) {
-                    bricks.splice(i, 1);
-                } else {
-                    brick.energy -= 1;
-                }
-                colliding = true;
-            }
-        });
-        return colliding;
+        if (brick.energy === 1) {
+          bricks.splice(index, 1);
+        } else {
+          brick.energy -= 1;
+        }
+        colliding = true;
+      }
+    });
+    return colliding;
+  }
+
+  checkBallCollision(ball: Ball, paddle: Paddle, view: CanvasView): void {
+    // 1. Check ball collision with paddle
+    if (
+      ball.getPosition().x + ball.getWidth() > paddle.getPosition().x &&
+      ball.getPosition().x < paddle.getPosition().x + paddle.getWidth() &&
+      ball.getPosition().y + ball.getWidth() === paddle.getPosition().y
+    ) {
+      ball.changeYDirection();
     }
-
-    checkBallCollision(ball: Ball, paddle: Paddle, view: CanvasView): void {
-        // 1. Check ball collision with paddle
-        if (
-            ball.pos.x + ball.width > paddle.pos.x &&
-            ball.pos.x < paddle.pos.x + paddle.width &&
-            ball.pos.y + ball.height === paddle.pos.y
-        ) {
-            ball.changeYDirection();
-        }
-        // 2. Check ball collision with walls
-        // Ball movement X constraints
-        if (ball.pos.x > view.canvas.width - ball.width || ball.pos.x < 0) {
-            ball.changeXDirection();
-        }
-        // Ball movement Y constraints
-        if (ball.pos.y < 0) {
-            ball.changeYDirection();
-        }
+    // 2. Check ball collision with walls
+    // Ball movement X constraints
+    if (
+      ball.getPosition().x > view.canvas.width - ball.getWidth() ||
+      ball.getPosition().x < 0
+    ) {
+      ball.changeXDirection();
     }
+    // Ball movement Y constraints
+    if (ball.getPosition().y < 0) {
+      ball.changeYDirection();
+    }
+  }
 }
